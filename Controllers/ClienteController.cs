7ff76e7e531bf458608f61e01109de4a32d447fb
Ester <ec747099@gmail.com>
2025-06-16 22:27:27 -0300

    Primeiro commit do projeto

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,77 @@
+﻿using CadastroClientes.Data;
+using CadastroClientes.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CadastroClientes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ClienteController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Clientes.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cliente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var cliente = await _context.Clientes.FindAsync(id);
+            if (cliente == null) return NotFound();
+            return View(cliente);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Cliente cliente)
+        {
+            if (id != cliente.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(cliente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cliente = await _context.Clientes.FindAsync(id);
+            if (cliente == null) return NotFound();
+            return View(cliente);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cliente = await _context.Clientes.FindAsync(id);
+            _context.Clientes.Remove(cliente);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CadastroClientes.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [StringLength(15)]
+        public string Telefone { get; set; }
+    }
+}
